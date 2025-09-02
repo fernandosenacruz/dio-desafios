053fe69ec8de3950f31af6fdc0bce070629c6428
Fernado Sena Cruz <fernandosenacruz@gmail.com>
2025-09-02 08:04:00 -0300

    feat: seed initial admin data in the database

--- dotnet-desafio-minimals-apis/Minimal-API/Infrastructure/Db/AppDbContext.cs
+++ dotnet-desafio-minimals-apis/Minimal-API/Infrastructure/Db/AppDbContext.cs
@@ -7,6 +7,19 @@ namespace MinimalApi.Infrastructure.Db
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Admin>().HasData(
+                new Admin
+                {
+                    Id = 1,
+                    Profile = "Admin",
+                    Email = "admin1@example.com",
+                    Password = "123456"
+                }
+            );
+        }
+
         public DbSet<Admin> Admins { get; set; } = default!;
     }
 }
